40bad81d9334e7a2d71b4f6881668fb72368664c
Gabriel Dias <gabriel.amaraldias03@gmail.com>
2023-06-13 11:13:47 -0300

    Primeiro

---
+++ piloto/exrcicio1.py
@@ -0,0 +1,21 @@
+import math
+frase = input("Digite um frase para o salvamento:")
+n1 = len(frase)
+n2 = frase.count("a")
+calculo = n1 + n2
+caclculo2 = math.radians(calculo)
+print("Quantidade de carcteres é ", n1)
+print("Quantidade de carcteres é 'a'",n2)
+print("Soma dos carcateres é ",calculo)
+print("Ovalor do radiando é  ",caclculo2)
+print(frase.split())
+print("""O Texas é um grande estado no sul dos EUA com desertos,
+ florestas de pinheiros e o rio Grande, que delimita a fronteira com o México. 
+ Em sua maior cidade, Houston,
+   o Museu de Belas-artes abriga obras de pintores impressionistas e renascentistas conhecidos, enquanto o Space Center Houston oferece exposições interativas projetadas pela NASA. Austin, a capital,
+ é conhecida pelo seu cenário musical eclético e pela LBJ Presidential Library.""")
+
+
+
+
+
